1cb31b75ac839213328543f3e01aa796ba9a2d0e
Surya-0804 <surya.abothula@gmail.com>
2025-01-29 12:32:50 +0530

    Adding the appointments count for superadmin

--- models/appointments-visits/appointments.js
+++ models/appointments-visits/appointments.js
@@ -1,53 +1,57 @@
 import mongoose from 'mongoose';
 
-const appointmentSchema = new mongoose.Schema({
+const appointmentSchema = new mongoose.Schema(
+  {
     hcmId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'causers'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'causers',
     },
     tenantId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'causers'
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'causers',
     },
     date: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     startTime: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     endTime: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true,
     },
     activity: {
-        type: String
+      type: String,
     },
     methodOfContact: {
-        type: String,
-        enum: ['in-person', 'remote'],
+      type: String,
+      enum: ['in-person', 'remote'],
     },
     reasonForRemote: {
-        type: String,
-        required: function () {
-            return this.methodOfContact === 'remote';
-        }
+      type: String,
+      required: function () {
+        return this.methodOfContact === 'remote';
+      },
     },
     placeOfService: {
-        type: String,
+      type: String,
     },
     serviceType: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     status: {
-        type: String,
-        enum: ['pending', 'completed'],
-        default: 'pending'
-    }, companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'company' }
-}, { timestamps: true });
+      type: String,
+      enum: ['pending', 'completed', 'cancelled'],
+      default: 'pending',
+    },
+    companyId: { type: mongoose.Schema.Types.ObjectId, ref: 'company' },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model('appointments', appointmentSchema);
--- routes/account/superAdminRoute.js
+++ routes/account/superAdminRoute.js
@@ -1,22 +1,41 @@
 import express from 'express';
-import { getCompanyReports, updateCompanyData, deleteCompany, getSuperAdminData } from '../../controllers/account/superAdminController.js';
+import {
+  getCompanyReports,
+  updateCompanyData,
+  deleteCompany,
+  getSuperAdminData,
+  getAllVisitsCount,
+  getAllAppointmentsCount,
+} from '../../controllers/account/superAdminController.js';
 import { authenticateToken } from '../../middleware/auth.js';
-
 
 const router = express.Router();
 
 // router.get('/get-super-admin-details', authenticateToken, getSuperAdminDetails);
 
 //superadmin reports
-router.get("/get-company-reports", authenticateToken, getCompanyReports);
+router.get('/get-company-reports', authenticateToken, getCompanyReports);
 
 //update company data
-router.put("/update-company-data/", authenticateToken, updateCompanyData);
+router.put('/update-company-data/', authenticateToken, updateCompanyData);
 
 //delete company
-router.delete("/delete-company/:companyId", authenticateToken, deleteCompany);
+router.delete('/delete-company/:companyId', authenticateToken, deleteCompany);
 
 //superadmin data
-router.get("/get-super-admin-data/:adminId", authenticateToken, getSuperAdminData);
+router.get(
+  '/get-super-admin-data/:adminId',
+  authenticateToken,
+  getSuperAdminData
+);
 
+//visits count of all companies
+router.get('/get-all-visits-count', authenticateToken, getAllVisitsCount);
 export default router;
+
+//appointments count of all companies
+router.get(
+  '/get-all-appointments-count',
+  authenticateToken,
+  getAllAppointmentsCount
+);
